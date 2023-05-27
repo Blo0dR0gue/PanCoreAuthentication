6f961334bddc9f0a3e7920d50026857921055f1f
Daniel Czeschner <daniel.czeschner@web.de>
2023-05-27 23:41:46 +0200

    Added config

---
+++ src/main/java/de/panomenal/core/authentication/config/AuthConfiguration.java
@@ -0,0 +1,38 @@
+package de.panomenal.core.authentication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import dev.samstevens.totp.secret.DefaultSecretGenerator;
+import dev.samstevens.totp.secret.SecretGenerator;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true // Allow role check on method entry
+)
+public class AuthConfiguration {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        // Setup the encoder for passwords
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecretGenerator secretGenerator() {
+        return new DefaultSecretGenerator();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        // Setting up auth manager
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
