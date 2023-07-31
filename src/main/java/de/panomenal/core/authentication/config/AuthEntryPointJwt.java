bdf81bb6ddd2b51975a89ab6c07e76c8c2b85da5
Daniel Czeschner <daniel.czeschner@web.de>
2023-07-31 21:02:34 +0200

    Reworked unauthorized handler

--- src/main/java/de/panomenal/core/authentication/config/AuthEntryPointJwt.java
+++ src/main/java/de/panomenal/core/authentication/config/AuthEntryPointJwt.java
@@ -1,13 +1,20 @@
 package de.panomenal.core.authentication.config;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.panomenal.core.authentication.auxiliary.data.response.ApiErrorResponse;
 
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
@@ -18,8 +25,17 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
             HttpServletResponse response,
             AuthenticationException authException) throws IOException {
 
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-                "You need to provide the JWT Token to Access This resource");
+        // response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+        // "You need to provide the JWT Token to Access This resource");
+
+        ApiErrorResponse re = new ApiErrorResponse(HttpStatus.UNAUTHORIZED.value(), "Authentication failed",
+                "You have to authenticate yourself via a post-request through the login endpoint.");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        OutputStream responseStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(responseStream, re);
+        responseStream.flush();
     }
 
 }
